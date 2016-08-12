f5a54b4667c069b132d3011738ac5358a62e6f88
eozan <erdemozanozbek@gmail.com>
2016-08-13 01:33:49 +0200

    -

--- tests/main.js
+++ tests/main.js
@@ -260,3 +260,91 @@ test('setFgColor', function(t) {
 
     t.end();
 });
+
+test('setStyle', function(t) {
+    let stream = new MockStream(4, 4);
+    let ts = new TerminalScreen(stream);
+    let spy = sinon.spy(ts, '_escape');
+
+    ts.setStyle('underline', undefined, true);
+    t.ok(ts.options.styles.has('underline'), 'sets default');
+
+    ts.setStyle('bold');
+    t.ok(ts.options.styles.has('bold'), 'sets fgColor');
+    t.equal(
+        spy.withArgs(codes.styles.bold[1]).callCount,
+        1,
+        'writes code'
+    );
+
+    ts.setStyle('bold');
+    t.equal(
+        spy.withArgs(codes.styles.bold[1]).callCount,
+        1,
+        'compares current'
+    );
+
+    ts.setStyle('bold', true, true);
+    t.equal(
+        spy.withArgs(codes.styles.bold[1]).callCount,
+        2,
+        'forces change'
+    );
+
+    t.equal(ts, ts.setStyle('bold'), 'returns instance');
+
+    t.end();
+});
+
+test('setStyles', function(t) {
+    let stream = new MockStream(4, 4);
+    let ts = new TerminalScreen(stream);
+    let spy = sinon.spy(ts, '_escape');
+    let spyStyle = sinon.spy(ts, 'setStyle');
+    let spyBg = sinon.spy(ts, 'setBgColor');
+    let spyFg = sinon.spy(ts, 'setFgColor');
+
+    ts.setStyles(['bold', 'underline']);
+    t.equal(
+        spy.withArgs(codes.styles.reset).callCount,
+        1,
+        'resets styles'
+    );
+    t.equal(spyBg.callCount, 1, 'fixes bgColor');
+    t.equal(spyFg.callCount, 1, 'fixes fgColor');
+    t.equal(spyStyle.withArgs('bold', true).callCount, 1, 'sets style 1');
+    t.equal(spyStyle.withArgs('underline', true).callCount, 1, 'sets style 2');
+
+    ts.setStyles(['dim'], true);
+    t.equal(spyStyle.withArgs('dim', true, true).callCount, 1, 'forces change');
+
+    t.equal(ts, ts.setStyles(['dim']), 'returns instance');
+
+    t.end();
+});
+
+test('setOptions', function(t) {
+    let stream = new MockStream(4, 4);
+    let ts = new TerminalScreen(stream);
+    let options = {
+        encoding: 'ascii',
+        cursor: false,
+        x: 3,
+        y: 2,
+        bgColor: 'red',
+        fgColor: 'blue',
+        styles: ['reverse', 'blink'],
+        wrap: false,
+        scroll: false,
+        lock: true
+    };
+
+    ts.setOptions(options);
+    t.deepLooseEqual(
+        ts.options,
+        Object.assign({}, options, {styles: new Set(options.styles)}),
+        'sets options'
+    );
+
+    t.end();
+});
